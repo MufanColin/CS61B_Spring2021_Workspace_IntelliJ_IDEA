84970342b6e539fd6775bf8a494422d196f88e1f
MufanColin <76479709+MufanColin@users.noreply.github.com>
2023-02-14 14:34:14 +0800

    some changes

--- Lecture04_References_Recursion_Lists/IntList.java
+++ Lecture04_References_Recursion_Lists/IntList.java
@@ -1,15 +1,67 @@
 public class IntList {
     public int first;
     public IntList rest;
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+    public int getSizeUsingRecursion() {
+        if (this.rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.getSizeUsingRecursion();
+    }
+
+    public int getSizeUsingIteration() {
+        IntList p = this;
+        int size = 0;
+        while (p != null) {
+            size += 1;
+            p = p.rest;
+        }
+        return size;
+    }
+
+    public int getUsingRecursion(int index) {
+        if (this.getSizeUsingIteration() <= index) {
+            System.out.println("Error: out of range!!!");
+            return -1;
+        }
+        if (index == 0) {
+            return this.first;
+        }
+        return this.rest.getUsingRecursion(index - 1);
+    }
+    public int getUsingIteration(int index) {
+        if (this.getSizeUsingIteration() <= index) {
+            System.out.println("Error: out of range!!!");
+            return -1;
+        }
+        IntList p = this;
+        int currentIndex = 0;
+        while (currentIndex < index) {
+            p = p.rest;
+            currentIndex += 1;
+        }
+        return p.first;
+    }
+
     public static void main(String[] args) {
-        IntList L = new IntList();
-        L.first = 5;
-        L.rest = new IntList();
-        L.rest.first = 10;
-        L.rest.rest = new IntList();
-        L.rest.rest.first = 15;
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
         System.out.println(L.first);
         System.out.println(L.rest.first);
         System.out.println(L.rest.rest.first);
+        System.out.println(L.getSizeUsingRecursion());
+        System.out.println(L.getSizeUsingIteration());
+        System.out.println(L.getUsingIteration(0));
+        System.out.println(L.getUsingIteration(1));
+        System.out.println(L.getUsingIteration(2));
+        System.out.println(L.getUsingIteration(100));
+        System.out.println(L.getUsingRecursion(0));
+        System.out.println(L.getUsingRecursion(1));
+        System.out.println(L.getUsingRecursion(2));
+        System.out.println(L.getUsingRecursion(100));
     }
 }
